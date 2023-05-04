e87674355ae0242ad3c18b963f292bd4c36877da
daiquoctuan <anhtuan6923@gmail.com>
2023-05-04 13:07:39 +0700

    4.5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 // import Baitap1 from "./Baitap1"
-import Baitap2 from './Baitap2';
+// import Baitap2 from './Baitap2';
+import RenderingElement from './rendering-elements';
 function App() {
   return (
     <div>
       {/* <Baitap1/> */}
-      <Baitap2/>
+      {/* <Baitap2 /> */}
+      <RenderingElement />
     </div>
   )
 }
---
+++ src/rendering-elements/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class RenderingElement extends Component {
+    username = "cybersoft"
+    age = 5
+
+    renderInfo() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>RenderingElement</h3>
+
+            </div>
+        )
+    }
+}
